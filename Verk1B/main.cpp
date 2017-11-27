4034a28eb37f2d09b450faa439ee1124339e5697
root <root@LAPTOP-MEPANN9I.localdomain>
2017-11-27 17:44:27 +0000

    B hluti buid

--- Verk1B/main.cpp
+++ Verk1B/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str;
+    ofstream fout;
+    fout.open("TextFile.txt");
+
+    getline(cin, str);
+    while (str[0] != '\\')
+    {
+        cout << str << endl;
+        getline(cin, str);
+    }
+
+    fout.close();
+
     return 0;
 }

767aabad3600aff37d71e5d7249c825b44c2ec69
root <root@LAPTOP-OL9PC8NL.localdomain>
2017-11-30 15:55:26 +0000

    added to pizza class

--- Pizza/3vikuverk/src/Pizza.cpp
+++ Pizza/3vikuverk/src/Pizza.cpp
@@ -2,7 +2,9 @@
 
 Pizza::Pizza()
 {
-    //ctor
+    _amount = 0;
+    _size_of_pizza = "";
+    _sauce = "";
 }
 
 Pizza::~Pizza()
@@ -11,7 +13,92 @@ Pizza::~Pizza()
 }
 
 void Pizza::make_pizza(){
-    Toppings pizza;
-    pizza.add_toppings();
+    Pizza pizza;
+    Toppings add_toppings;
+    char input = '0';
+    int amount = 0;
+    string add;
+    while(input != 'q' || input != 'Q'){
+        cout << "what kind of topping would you like to add?" << endl;
+        cout << "Size = i | " << "Sauce = s | " << "Toppings = t | " << "Quit = q" << endl;
+        cout << "Picked toppings: " << pizza._topping_on_pizza;
+        cout << "Picked size: " << pizza._size_of_pizza << endl;
+        cout << "picked sauce: " << pizza._sauce << endl;
+        cout << "Pizza cost: " << pizza._amount << endl;
+        cout << "Type: ";
+        cin >> input;
+        if(input == 'i' || input == 'I'){
+            pizza.display_sizes(add, amount);
+            pizza._amount = amount;
+            pizza._size_of_pizza = add;
+        }
+        else if(input == 's' || input == 'S'){
+            pizza.display_sauce();
+        }
+        else if(input == 't' || input == 'T'){
+            system("CLS");
+            pizza._topping_on_pizza = add_toppings.add_toppings();
+            pizza._amount += add_toppings.get_amount(pizza._topping_on_pizza);
+        }
+        else if(input == 'q' || input == 'Q'){
+            break;
+        }
+        else{
+            cout << "Invalid input!" << endl;
+        }
+        system("CLS");
+    }
+
+}
+
+void Pizza::display_sizes(string& add,int& amount){
+    ifstream fin;
+    string st;
+    bool pick = true;
+    int counter = 1;
+    int number = 0;
+    int price = 1000;
+    fin.open("pizza_sizes.txt");
+    if(fin.is_open()){
+        cout << "List of Sizes" << endl;
+        while(!fin.eof()){
+            cout << counter << ". ";
+            getline(fin, st);
+            cout << st << " " << price << ".kr" << endl;
+            price += 200;
+            counter++;
+        }
+    }
+    fin.close();
+    while(pick == true){
+        cout << "What size would you like: ";
+        cin >> number;
+        if(number > 0 && number < counter){
+            pick = false;
+        }
+        else{
+            cout << "Invalid input!" << endl;
+        }
+    }
+
+    counter = 1;
+    price = 1000;
+    fin.open("pizza_sizes.txt");
+    if(fin.is_open()){
+        while(!fin.eof()){
+            getline(fin, st);
+            if(counter == number){
+                add = st;
+                amount = price;
+            }
+            price += 200;
+            counter++;
+        }
+    }
+    fin.close();
+
+}
+
+void Pizza::display_sauce(){
 
 }
--- Pizza/3vikuverk/src/Toppings.cpp
+++ Pizza/3vikuverk/src/Toppings.cpp
@@ -5,12 +5,18 @@ Toppings::Toppings()
     _amount = 0;
 }
 
+Toppings::~Toppings()
+{
+    //dtor
+}
+
 //this function prompts the toppings for the user
 void Toppings::display_toppings(char input, int price, string& topping){
     ifstream fin;
     string st;
     int counter = 1;
     int number = 0;
+    bool pick = true;
 //depending on what the user input the program opens the file that he asked for
     if(input == 'M' || input == 'm'){
         fin.open("Meat.txt");
@@ -39,8 +45,16 @@ void Toppings::display_toppings(char input, int price, string& topping){
         cout << "Unable to open documment" << endl;
     }
 // here the user inputs the topping that he wants of the list
-    cout << "Pick a Topping: ";
-    cin >> number;
+    while(pick == true){
+        cout << "Pick a Topping: ";
+        cin >> number;
+        if(number > 0 && number < counter){
+            pick = false;
+        }
+        else{
+            cout << "Invalid input!" << endl;
+        }
+    }
     fin.close();
 // in the code below im running through the file and adding the topping that the user
 // picked to the topping variable so I can later add it to the name of topping vector
@@ -79,19 +93,10 @@ Toppings Toppings::add_toppings(){
         cout << "what kind of topping would you like to add?" << endl;
         cout << "Cheese = c | " << "Meat =  m | " << "Vegetables =  v | " << "Quit = q" << endl;
         cout << "picked toppings: ";
-// in the for loop the toppings that the user has picked get printed out on the screen  for him to view
-        for(unsigned int i = 0; i < make_pizza._name_of_topping.size(); i++){
-            if(i == (make_pizza._name_of_topping.size()-1)){
-                cout << make_pizza._name_of_topping[i] << " ";
-            }
-            else{
-                cout << make_pizza._name_of_topping[i] << ", ";
-            }
-        }
-// here the program prints out the total cost of all the toppings that the user has picked
-        cout << endl << "Total amount: " << make_pizza._amount << endl;
-        cout << "Type: ";
+        cout << make_pizza;
+        cout << "Topping cost: " << make_pizza._amount << ".kr" << endl;
 // here the user picks what kind of topping he wants to add or stop
+        cout << "Type: ";
         cin >> input;
         if(input == 'M' || input == 'm'){
             price = 50;
@@ -104,6 +109,8 @@ Toppings Toppings::add_toppings(){
         }
         if(input == 'M' || input == 'm' || input == 'V' || input == 'v' || input == 'C' || input == 'c'){
             display_toppings(input, price, topping);
+            make_pizza._amount += price;
+            make_pizza._name_of_topping.push_back(topping);
         }
         else if(input == 'q' || input == 'Q'){
             break;
@@ -111,17 +118,28 @@ Toppings Toppings::add_toppings(){
         else{
             cout << "Invalid input!" << endl;
         }
-// here the price gets added to the total amount
-        make_pizza._amount += price;
-// here the topping that the user picked gets added to the name of toppings list
-// so that it can be printed out later
-        make_pizza._name_of_topping.push_back(topping);
         system("CLS");
     }
     return make_pizza;
 }
 
-Toppings::~Toppings()
-{
-    //dtor
+ostream& operator << (ostream& out, Toppings top){
+    for(unsigned int i = 0; i < top._name_of_topping.size(); i++){
+            if(i == (top._name_of_topping.size()-1)){
+                out << top._name_of_topping[i] << " ";
+            }
+            else{
+                out << top._name_of_topping[i] << ", ";
+            }
+        }
+        out << endl;
+    return out;
 }
+
+int Toppings::get_amount(Toppings amount){
+    int cost = 0;
+    cost = amount._amount;
+
+    return cost;
+}
+

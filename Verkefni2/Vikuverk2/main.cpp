cdf2b11cb65caa7609a00629542a6d24a7c84263
root <root@LAPTOP-MEPANN9I.localdomain>
2017-12-05 15:17:32 +0000

    Egill laga main

--- Verkefni2/Vikuverk2/main.cpp
+++ Verkefni2/Vikuverk2/main.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
-
 #include "Main_ui.h"
-
 #include "Employee.h"
+#include "Validate.h"
 
 
 using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    string name = "Egill Aron thorisson";
-    string ssn = "4273723684";
-    double salary = 599.08;
-    int month = 06;
-    int year = 2017;
 
-    Validate bla;
-    cout << bla.validateAll (name, ssn, salary, month, year);
-=======
     Employee lala;
     cin >> lala;
     cout << endl << lala << endl;
->>>>>>> d3ed6d3233c0667c4d21e45c88ef5fc59511bd84
+
 
     return 0;
 }

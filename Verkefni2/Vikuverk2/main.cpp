0b55bb26e04676950379faa2c1cfda5ba85c6913
root <root@LAPTOP-MEPANN9I.localdomain>
2017-12-05 15:12:58 +0000

    Egill sækja

--- Verkefni2/Vikuverk2/main.cpp
+++ Verkefni2/Vikuverk2/main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main()
 {
-    string name = "Egill Aron";
+    string name = "Egill Aron thorisson";
     string ssn = "4273723684";
     double salary = 599.08;
     int month = 06;
     int year = 2017;
 
     Validate bla;
-    cout << bla.validateAll(name, ssn, salary, month, year);
+    cout << bla.validateAll (name, ssn, salary, month, year);
 
     return 0;
 }

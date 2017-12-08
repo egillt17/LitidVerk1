ef8f268e9ac8451c757afb8a85a260e2e6d7ab1f
root <root@LAPTOP-VN9N3OB4.localdomain>
2017-12-08 14:39:13 +0000

    njalli 2

--- Pizza2/Stortverk/main.cpp
+++ Pizza2/Stortverk/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include "Display.h"
+#include "Manager.h"
+#include "Sales.h"
+#include "Baker.h"
+#include "Delivery.h"
+#include "Pizza.h"
+#include "Order.h"
+#include "Toppings.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Pizza!" << endl;
+
+    Display mainUI;
+    mainUI.displayMainUI();
+
+    Pizza lala;
+    Pizza grgr;
+    lala = lala.make_pizza();
+    grgr = grgr.make_pizza();
+    system("CLS");
+    cout << "This is your order: " << endl;
+    cout << lala;
+    cout << grgr;
+
     return 0;
 }

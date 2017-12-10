415411b89318012a8f342aa025195850294a2329
root <root@LAPTOP-MEPANN9I.localdomain>
2017-12-10 19:30:14 +0000

    Egill order - UI vinna saman

--- Pizza2/Stortverk/main.cpp
+++ Pizza2/Stortverk/main.cpp
@@ -6,12 +6,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-=======
-
-//    Order order;
-//    cout << order << endl << endl;
->>>>>>> 34ac3a697acf0367465bdaf50eb43fc8afedf5ee
     Main_ui mainUI;
     mainUI.mainUI();
 

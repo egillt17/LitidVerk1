beaa0a81766c1a43de7475c60fd7f1b8eda07961
root <root@LAPTOP-MEPANN9I.localdomain>
2017-12-11 19:59:00 +0000

    Egill na i

--- Pizza2/Stortverk/include/models/Order.h
+++ Pizza2/Stortverk/include/models/Order.h
@@ -9,29 +9,53 @@ class Order
     public:
         Order();
         vector<Pizza> getPizzas();
+        ///Get function that return the variable pizzas
         vector<Toppings> getExtras();
+        ///Get function that return the variable Extras
         string getPerson();
+        ///Get function that return the variable Person
         string getLocation();
+        ///Get function that return the variable Location
         string getComments();
+        ///Get function that return the variable Comments
         char getProgress();
+        ///Get function that return the variable Progress
         char getPickedOrSent();
+        ///Get function that return the variable PickedUpOrSent
         char getDelivered();
+        ///Get function that return the variable Delivered
         char getPrice();
+        ///Get function that return the variable Price
         char getPaidFor();
+        ///Get function that return the variable PaidFor
         int getPizzaTotalCost();
+        ///Get function that return the variable PizzatotalCost
         int getExtrasTotalCost();
+        ///Get function that return the variable ExtraTotalCost
         void addPizza(Pizza pizza);
+        ///set function that adds the pizza that was sent to the function to the vector pizzas
         void addExtras(Toppings extra);
+        ///set function that adds the extra that was sent to the function to the vector extras
         void setPerson(string name);
+        ///Set function that sets the Person varialbe with the string that was sent to the function
         void setLocationForOrder(string location);
+        ///Set function that sets the location varialbe with the string that was sent to the function
         void setComments(string comments);
+        ///Set function that sets the comments varialbe with the string that was sent to the function
         void setProgress(char inProgress);
+        ///Set function that sets the progress varialbe with the char that was sent to the function
         void setPickedOrSent(char pickedOrSent);
+        ///Set function that sets the PickedOrSent varialbe with the char that was sent to the function
         void setDelivered(char delivered);
+        ///Set function that sets the Delivered varialbe with the char that was sent to the function
         void setPrice(int price);
+        ///Set function that sets the Price varialbe with the int that was sent to the function
         void setPaidFor(char paidFor);
+        ///Set function that sets the PaidFor varialbe with the chat that was sent to the function
         friend ostream& operator << (ostream& out, Order& order);
+        ///Function that overleads the ostream operator
         friend ofstream& operator << (ofstream& out, Order& order);
+        ///Function that overleads the ofstream operator
 
     private:
         vector<Pizza> _pizza;

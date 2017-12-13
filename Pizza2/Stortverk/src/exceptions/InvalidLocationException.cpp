f373d7239a37aee00a73914b0f2bc658060837a5
root <root@LAPTOP-VN9N3OB4.localdomain>
2017-12-13 17:55:42 +0000

    comments

--- Pizza2/Stortverk/src/exceptions/InvalidLocationException.cpp
+++ Pizza2/Stortverk/src/exceptions/InvalidLocationException.cpp
@@ -1,10 +1,12 @@
 #include "InvalidLocationException.h"
 
+/// constructor, takes the message and puts in private variable
 InvalidLocationException::InvalidLocationException(string message)
 {
     _message = message;
 }
 
+/// getMessage returns the message stored in the private message variable
 string InvalidLocationException::getMessage() {
     return _message;
 }
--- Pizza2/Stortverk/src/exceptions/InvalidNameException.cpp
+++ Pizza2/Stortverk/src/exceptions/InvalidNameException.cpp
@@ -1,10 +1,12 @@
 #include "InvalidNameException.h"
 
+/// constructor, takes the message and puts in private variablea
 InvalidNameException::InvalidNameException(string message)
 {
     _message = message;
 }
 
+/// getMessage returns the message stored in the private message variable
 string InvalidNameException::getMessage() {
     return _message;
 }

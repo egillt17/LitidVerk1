314f1928d689569380100d2b57694d7e2e353bdf
root <root@LAPTOP-OL9PC8NL.localdomain>
2017-12-14 22:34:18 +0000

    commented pizza service and repo

--- Pizza2/Stortverk/include/repository/PizzaRepo.h
+++ Pizza2/Stortverk/include/repository/PizzaRepo.h
@@ -10,14 +10,23 @@ class PizzaRepo
 {
     public:
         vector <string> readLocations();
+        /// reads all locations from the file
         void addLocationToList(string location);
+        /// adds a location to the file
         void reWriteLocationList(vector <string> locations);
+        /// rewrites the location file
         void addOrderToList(Order order);
+        /// adds an order to the file
         vector <Order> readOrders();
+        /// reads all orders from the file
         void reWriteOrders(vector <Order> orders);
+        /// rewrites order file
         vector <string> readPizzaSpecials();
+        /// reads all pizza specials from file
         void addPizzaSpecial(Pizza pizza);
+        /// adds pizza special from file
         void reWritePizzaSpecial(vector <Pizza> pizzas);
+        /// rewrites pizza special file
 
     private:
 };
--- Pizza2/Stortverk/src/repository/PizzaRepo.cpp
+++ Pizza2/Stortverk/src/repository/PizzaRepo.cpp
@@ -4,52 +4,52 @@ vector <string> PizzaRepo::readLocations() {
     vector <string> locations;
     string location;
     ifstream fin;
-    fin.open("Locations.txt");
-    if(fin.is_open()) {
-        while(getline(fin, location)) {
-            locations.push_back(location);
+    fin.open("Locations.txt");  /// opens location file
+    if(fin.is_open()) { /// checks if open
+        while(getline(fin, location)) { /// gets all the info from the file
+            locations.push_back(location);  /// adds it to the vector
         }
     }
-    fin.close();
-    return locations;
+    fin.close();    ///closes file
+    return locations;   /// returns vector
 }
 
 void PizzaRepo::addLocationToList(string location) {
     ofstream fout;
-    fout.open("Locations.txt", ios::app);
-    if(fout.is_open()) {
-        fout << location << endl;
+    fout.open("Locations.txt", ios::app);   /// opens location file and adds to the end of it
+    if(fout.is_open()) {    /// checks if its open
+        fout << location << endl;   /// adds the location to the end of the file
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// closes file
 }
 
 void PizzaRepo::reWriteLocationList(vector <string> locations) {
     ofstream fout;
-    fout.open("Locations.txt");
-    if(fout.is_open()) {
-        for(unsigned int i = 0; i < locations.size(); i++) {
-            fout << locations[i] << endl;
+    fout.open("Locations.txt"); /// opens location file
+    if(fout.is_open()) {    /// checks if its open
+        for(unsigned int i = 0; i < locations.size(); i++) {    /// goes through the vector with all the locations
+            fout << locations[i] << endl;   /// and adds all the locations to the file
         }
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// closes file
 }
 
 void PizzaRepo::addOrderToList(Order order) {
     ofstream fout;
-    fout.open("Orders.txt", ios::app);
-    if(fout.is_open()) {
-        fout << order;
+    fout.open("Orders.txt", ios::app);  /// opens order file
+    if(fout.is_open()) {    /// checks if its open
+        fout << order;  /// adds the order at the end of the file
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// close file
 }
 
 vector <Order> PizzaRepo::readOrders() {
@@ -59,75 +59,75 @@ vector <Order> PizzaRepo::readOrders() {
     ifstream fin;
     string st;
     vector <string> info;
-    fin.open("Orders.txt");
-    if(fin.is_open()) {
-        while(!fin.eof()) {
-            getline(fin, st);
-            if(st == ":") {
-                tempOrder = get.fixOrderInfo(info);
-                orders.push_back(tempOrder);
-                info.clear();
+    fin.open("Orders.txt"); /// opens order file
+    if(fin.is_open()) { /// checks if its open
+        while(!fin.eof()) { /// loop runs til it reaches the end of the file
+            getline(fin, st);   /// gets each line from the file
+            if(st == ":") { /// checks for delimiter
+                tempOrder = get.fixOrderInfo(info); /// sends the info to be fixed and then added to a temp order variable
+                orders.push_back(tempOrder);    /// adds the fixed order variable to the order vector
+                info.clear();   /// clears the vector that had the info
             }
-            else {
+            else {  /// if it doesnt find the delimiter then it adds to the info vector
                 info.push_back(st);
             }
         }
     }
-    fin.close();
-    return orders;
+    fin.close();    /// closes file
+    return orders;  /// returns the orders
 }
 
 void PizzaRepo::reWriteOrders(vector <Order> orders) {
     ofstream fout;
-    fout.open("Orders.txt");
-    if(fout.is_open()) {
-        for(unsigned int i = 0; i < orders.size(); i++) {
-            fout << orders[i];
+    fout.open("Orders.txt");    /// opens order file
+    if(fout.is_open()) {    /// checks if open
+        for(unsigned int i = 0; i < orders.size(); i++) {   /// goes through all the orders
+            fout << orders[i];  /// and re writes the list with them
         }
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// file closes
 }
 
 vector <string> PizzaRepo::readPizzaSpecials() {
     vector <string> info;
     ifstream fin;
     string st;
-    fin.open("Pizza_specials.txt");
-    if(fin.is_open()) {
-        while(getline(fin, st)) {
-            info.push_back(st);
+    fin.open("Pizza_specials.txt"); /// opens pizza specials file
+    if(fin.is_open()) { /// checks if its open
+        while(getline(fin, st)) {   /// loop runs while there are lines to take out
+            info.push_back(st); /// adds the line to this vector
         }
     }
-    fin.close();
+    fin.close();    /// closes file
     return info;
 }
 
 void PizzaRepo::addPizzaSpecial(Pizza pizza) {
     ofstream fout;
-    fout.open("Pizza_specials.txt", ios::app);
-    if(fout.is_open()) {
-        fout << pizza;
+    fout.open("Pizza_specials.txt", ios::app);  /// opens pizza special file
+    if(fout.is_open()) {    /// checks if its open
+        fout << pizza;  /// adds the pizza at the end of the file
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// closes the file
 }
 
 void PizzaRepo::reWritePizzaSpecial(vector <Pizza> pizzas) {
     ofstream fout;
-    fout.open("Pizza_specials.txt");
-    if(fout.is_open()) {
-        for(unsigned int i = 0; i < pizzas.size(); i++) {
-            fout << pizzas[i];
+    fout.open("Pizza_specials.txt");    /// opens the pizza specials file
+    if(fout.is_open()) {    /// checks if its open
+        for(unsigned int i = 0; i < pizzas.size(); i++) {   /// goes through all the pizzas
+            fout << pizzas[i];  /// and re writes them in the file
         }
     }
     else {
 
     }
-    fout.close();
+    fout.close();   /// file closes
 }
 

d1aa35248efb3468276ed1e00a27a43d2244963e
root <root@LAPTOP-MEPANN9I.localdomain>
2017-12-14 18:04:38 +0000

    Egill models commentadir

--- Pizza2/Stortverk/include/services/TopService.h
+++ Pizza2/Stortverk/include/services/TopService.h
@@ -9,10 +9,13 @@ class TopService
 {
     public:
         vector <Toppings> getToppingList(int pick);
+        ///A function that returns the list of the extra that was picked
         void addTopping(string name, string price, int pick);
+        ///A function that takes in a name, price and type of extra and adds that to the appropriate .txt file
         bool removeTopping(string name, int pick);
+        ///A function that takes in a name and type of an extra, and removes that extra from the appropriate .txt file
         Toppings make_topping(string st, string price);
-
+        ///A function that is used when making a new extra, a name and a price is taken in
 
     private:
         bool toppingCheck(Toppings topping, int pick);
